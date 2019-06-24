68e8902970281ae5feff8f295038820577fc0d5d
mark price <pricehome@gmail.com>
2019-06-24 13:10:12 -0500

    branch commit for metals

---
+++ main.js
@@ -0,0 +1,34 @@
+const metals = [
+  { metal: "Sterling Silver", price: 12.42 },
+  { metal: "14K Gold", price: 736.4 },
+  { metal: "24K Gold", price: 1258.9 },
+  { metal: "Platinum", price: 795.45 },
+  { metal: "Palladium", price: 1241.0 }
+]
+
+const diamonds = [
+  { carets: 0.5, price: 405 },
+  { carets: 0.75, price: 782 },
+  { carets: 1, price: 1470 },
+  { carets: 1.5, price: 1997 },
+  { carets: 2, price: 3638 }
+]
+
+const ringStyles = [
+  { style: "Classic", price: 500 },
+  { style: "Modern", price: 710 },
+  { style: "Vintage", price: 965 }
+]
+
+const createCustomRing = (style, carets, metal) => {
+  return {
+      style: style.style,
+      carets: carets.carets,
+      metal: metal.metal,
+      price: metal.price + style.price + carets.price
+  }
+}
+
+const newRingOrder = createCustomRing(ringStyles[1], diamonds[2], metals[2])
+
+console.log(ringStyles[1], diamonds[2], metals[3])
